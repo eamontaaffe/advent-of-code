2d6f7680bec57880696d6167a0926bff3fb0ba01
Eamon Taaffe <eamontaaffe@gmail.com>
2019-12-07 22:44:27 +1100

    Day 3 part 1 - it works on examples

--- Day03Part1.hs
+++ Day03Part1.hs
@@ -1,6 +1,6 @@
 import Debug.Trace
 import Data.List.Split ( splitOn )
-import Data.List ( intersect, sort )
+import Data.List ( intersect, sort, nub )
 
 
 type Canvas =
@@ -27,7 +27,7 @@ sampleInput =
 -- >>> readInstruction "R8"
 -- GoRight 8
 readInstruction :: String -> Instruction
-readInstruction (dir:dist)
+readInstruction instruction@(dir:dist)
   | dir == 'U' = GoUp    $ read dist
   | dir == 'D' = GoDown  $ read dist
   | dir == 'L' = GoLeft  $ read dist
@@ -52,103 +52,47 @@ move (GoRight dist) (xPos, yPos) = (xPos + dist, yPos       )
 move (GoLeft  dist) (xPos, yPos) = (xPos - dist, yPos       )
 
 
--- >>> pathfinder [(0, 0)] (head $ parseInput sampleInput)
+-- >>> pathFinder [(0, 0)] (head $ parseInput sampleInput)
 -- [(3,2),(3,5),(8,5),(8,0),(0,0)]
-pathfinder :: [Location] -> [Instruction] -> [Location]
-pathfinder ls       []     = ls
-pathfinder ls@(l:_) (i:is) = pathfinder (move i l : ls) is
+pathFinder :: [Location] -> [Instruction] -> [Location]
+pathFinder ls       []     = ls
+pathFinder ls@(l:_) (i:is) = pathFinder (move i l : ls) is
 
 
--- >>> drawDot [[False, False], [False, False]] (1,0)
--- [[False,True],[False,False]]
-drawDot :: Canvas -> Location -> Canvas
-drawDot canvas (x, y)
-  =  take y canvas
-  ++ [take x (canvas !! y) ++ [True] ++ drop (x + 1) (canvas !! y)]
-  ++ drop (y + 1) canvas
-
-
--- >>> drawLine [[False, False], [False, False]] (1,0) (0,0)
--- [ [True,True]
--- , [False,False]
--- ]
-drawLine :: Canvas -> Location -> Location -> Canvas
-drawLine canvas (x1, y1) (x2, y2)
-  | x1 == x2 || y1 == y2 = foldl drawDot canvas dots
+-- >>> lineToDots (0, 0) (0, 3)
+-- [(0,0),(0,1),(0,2),(0,3)]
+lineToDots :: Location -> Location -> [Location]
+lineToDots (x1, y1) (x2, y2) =
+  [ (x, y) | x <- enumFromTo x1 x2, y <- enumFromTo y1 y2 ]
   where
-    dots = [ (x, y)
-           | x <- enumerateBetween x1 x2
-           , y <- enumerateBetween y1 y2
-           ]
+    enumFromTo :: Int -> Int -> [Int]
+    enumFromTo x y
+      | y > x     = [x..y]
+      | otherwise = [y..x]
 
 
--- >>> drawPath (initialiseCanvas 2 2) [(0,0), (1,0), (1, 1)]
--- [ [True,True]
--- , [False,True]
--- ]
-drawPath :: Canvas -> [Location] -> Canvas
-drawPath canvas (l:[]) = drawDot canvas l
-drawPath canvas ls@(l1:l2:_) =
-  drawPath (drawLine canvas l1 l2) (tail ls)
+-- >>> pathToDots [(0,0), (3,0)]
+-- [(0,0),(1,0),(2,0),(3,0)]
+pathToDots :: [Location] -> [Location]
+pathToDots ls@(l1:[]) = ls
+pathToDots ls@(l1:l2:_) =
+  lineToDots l1 l2 ++ pathToDots (tail ls)
 
 
--- >>> enumerateBetween 10 1
--- [1,2,3,4,5,6,7,8,9,10]
-enumerateBetween :: (Enum a, Ord a) => a -> a -> [a]
-enumerateBetween x y
-  | y > x     = [x..y]
-  | otherwise = [y..x]
-
-
--- HACK: Initialise the canvas to a fixed size
--- >>> initialiseCanvas 2 3
--- [ [False,False]
--- , [False,False]
--- , [False,False]
--- ]
-initialiseCanvas :: Int -> Int -> Canvas
-initialiseCanvas x y =
-  [ [ False | x <- [1..x] ] | y <- [1..y] ]
-
-
-initialCanvas :: Canvas
-initialCanvas =
-  initialiseCanvas 100 100
-
-
-intersectCanvas :: Canvas -> Canvas -> Canvas
-intersectCanvas xss yss =
-  [ [ x && y
-    | i <- [0 .. length xs - 1]
-    , let x = xs !! i
-    , let y = ys !! i
-    ]
-  | j <- [0 .. length xss  - 1]
-  , let xs = xss !! j
-  , let ys = yss !! j
-  ]
-
-
-canvasToDots :: Canvas -> [Location]
-canvasToDots xss =
-  concat [ [ (i, j)  | (i, x) <- zip [0..] xs, x ]
-         | (j, xs) <- zip [0..] xss
-         ]
-
-
+-- >>> locationToDistance (1, 3)
+-- 4
 locationToDistance :: Location -> Int
 locationToDistance (x, y) =
   abs x + abs y
-
 
 main :: IO ()
 main
   = interact
   $ show
-  . (flip (!!)) 1
+  . head
   . sort
+  . filter ((/=) 0)
   . map locationToDistance
-  . canvasToDots
-  . foldl1 intersectCanvas
-  . map (drawPath initialCanvas . pathfinder [(0, 0)])
+  . foldl1 intersect
+  . map (pathToDots . pathFinder [(0, 0)])
   . parseInput
